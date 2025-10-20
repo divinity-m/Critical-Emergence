0331f778c9e3c224d0bcd1f05b8117e602ecdfcc
Vasto <sonicdarkultra44@gmail.com>
2025-10-20 14:13:28 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,31 +2,66 @@
 const cnv = document.getElementById('canvas');
 const ctx = cnv.getContext('2d');
 
-function resizeCnv() {
-    cnv.width = window.innerWidth;
-    cnv.height = window.innerHeight;
-}
+function resizeCnv() { cnv.width = window.innerWidth; cnv.height = window.innerHeight; }
 resizeCnv();
 window.addEventListener('resize', resizeCnv);
 
-let angle = 0;
+// Controls
+document.addEventListener('keydown', keyDownEventListener);
+document.addEventListener('keyup', keyUpEventListener);
+let moveUp = false, moveDown = false, moveLeft = false, moveRight = false;
+function keyDownEventListener(e) {
+    if (e.code === "KeyW" || e.code === "ArrowUp") moveUp = true;
+    if (e.code === "KeyA" || e.code === "ArrowLeft") moveLeft = true;
+    if (e.code === "KeyS" || e.code === "ArrowDown") moveDown = true;
+    if (e.code === "KeyD" || e.code === "ArrowRight") moveRight = true;
+    if (e.code === "KeyQ") dash();
+}
+function keyUpEventListener(e) {
+    if (e.code === "KeyW" || e.code === "ArrowUp") moveUp = false;
+    if (e.code === "KeyA" || e.code === "ArrowLeft") moveLeft = false;
+    if (e.code === "KeyS" || e.code === "ArrowDown") moveDown = false;
+    if (e.code === "KeyD" || e.code === "ArrowRight") moveRight = false;
+}
+function keyboardMovement() {
+    if (moveUp) player.y -= player.speed;
+    if (moveLeft) player.x -= player.speed;
+    if (moveDown) player.y += player.speed;
+    if (moveRight) player.x += player.speed;
+}
+
+
+let player = {
+    x: cnv.width/2, y: cnv.height/2, r: 20, speed: 5, baseSpeed: 5, color: "#FFFFF", subColor: "#E6E6E6",
+}
+let now = Date.now();
+
 function draw() {
-    // Background
-    ctx.fillStyle = "rgb(200, 200, 200)";
+    now = Date.now();
+    // Background #RRGGBBAA
+    ctx.fillStyle = "#C8C8C8";
     ctx.fillRect(0, 0, cnv.width, cnv.height);
 
-    // Ball moving in a circle to lowky flex my animation skills lol
-    ctx.save();
-    ctx.translate(cnv.width/2, cnv.height/2);
-    ctx.rotate(angle);
-    angle += Math.PI / 20;
-    ctx.beginPath();
-    ctx.arc(10*Math.cos(Math.PI/4), 10*Math.sin(Math.PI/4), 5, Math.PI*2, 0);
-    ctx.fill();
-    ctx.restore();
-
-    if (angle >= Math.PI*2) angle = 0;
+    // Player
+    ctx.fillStyle = player.color;
+    ctx.strokeStyle = player.subColor;
+    ctx.lineWidth = 2;
+    circle(player.x, player.y, player.r, "fill");
+    circle(player.x, player.y, player.r, "stroke");
     
     requestAnimationFrame(draw);
 }
 draw()
+
+function cricle(x, y, r, type) {
+    ctx.beginPath();
+    ctx.arc(x, y, r, Math.PI*2, 0);
+    if (type === "stroke") ctx.stroke();
+    else ctx.fill();
+}
+function dash() {
+    console.log(dash);
+}
+
+
+
