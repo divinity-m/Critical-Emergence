c01c1f7202ac242fbacdcae164aa5c5fb3086a27
Vasto <sonicdarkultra44@gmail.com>
2025-10-22 19:38:16 -0600

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ window.addEventListener('resize', resizeCnv);
 
 // Variables
 let player = {
-    x: cnv.width/2, y: cnv.height/2, r: 15, speed: cnv.width/275, baseSpeed: cnv.width/275, color: "#FFFFFF", subColor: "#E6E6E6",
+    x: cnv.width*0.5, y: cnv.height*0.5, r: 15, speed: cnv.width*0.575, baseSpeed: cnv.width/275, color: "#FFFFFF", subColor: "#E6E6E6",
 }
 let now = Date.now();
 let mapY = 0, mapX = 0;
@@ -31,18 +31,36 @@ let dash = {
     }
 };
 
+// Mouse
+document.addEventListener('mousemove', mousemoveEventListener);
+document.addEventListener('click', clickEventListener);
+let cursorX = 0, cursorY = 0;
+let mouseover = {
+    equipSword: false,
+}
+function mousemoveEventListener(e) {
+    [cursorX, cursorY] = [e.clientX, e.clientY];
+}
+function detectHover() {
+    mouseover.equipSword = (mouseX > cnv.width*0.75-5+mapX && mouseX < cnv.width*0.75-5+mapX+110
+        && mouseY > cnv.height*0.5+60+mapY && mouseY < cnv.height*0.5+60+mapY + 20);
+}
+function clickEventListener(e) {
+    if (mouseover.equipSword) player.color = "#FF0000";
+}
+
 // Controls
-document.addEventListener('keydown', keyDownEventListener);
-document.addEventListener('keyup', keyUpEventListener);
+document.addEventListener('keydown', keydownEventListener);
+document.addEventListener('keyup', keyupEventListener);
 let moveUp = false, moveDown = false, moveLeft = false, moveRight = false;
-function keyDownEventListener(e) {
+function keydownEventListener(e) {
     if (e.code === "KeyW" || e.code === "ArrowUp") moveUp = true;
     if (e.code === "KeyA" || e.code === "ArrowLeft") moveLeft = true;
     if (e.code === "KeyS" || e.code === "ArrowDown") moveDown = true;
     if (e.code === "KeyD" || e.code === "ArrowRight") moveRight = true;
     if (e.code === "KeyQ" && now - dash.lastEnded > 1500) dash.activated = true;
 }
-function keyUpEventListener(e) {
+function keyupEventListener(e) {
     if (e.code === "KeyW" || e.code === "ArrowUp") moveUp = false;
     if (e.code === "KeyA" || e.code === "ArrowLeft") moveLeft = false;
     if (e.code === "KeyS" || e.code === "ArrowDown") moveDown = false;
@@ -76,6 +94,7 @@ function circle(x, y, r, type) {
 console.log("sword icon");
 function draw() {
     now = Date.now();
+    detectHover();
     // Background #RRGGBBAA
     ctx.fillStyle = "#C8C8C8";
     ctx.fillRect(0, 0, cnv.width, cnv.height);
@@ -100,16 +119,16 @@ function draw() {
         ctx.fillStyle = "#FFFFFF";
         ctx.lineWidth = 2;
         ctx.beginPath();
-        ctx.roundRect(cnv.width-175, cnv.height/2-6.25, 150, 12.5, 6.25);
+        ctx.roundRect(cnv.width-175, cnv.height*0.5-6.25, 150, 12.5, 6.25);
         ctx.stroke();
         ctx.beginPath();
-        ctx.roundRect(cnv.width-175, cnv.height/2-6.25, 150-(now-dash.lastEnded)/10, 12.5, 6.25);
+        ctx.roundRect(cnv.width-175, cnv.height*0.5-6.25, 150-(now-dash.lastEnded)/10, 12.5, 6.25);
         ctx.fill();
 
         ctx.fillStyle = "#E6E6E6";
         ctx.font = "15px Verdana";
         ctx.textAlign = "right";
-        ctx.fillText("Dash", cnv.width-185, cnv.height/2+5);
+        ctx.fillText("Dash", cnv.width-185, cnv.height*0.5+5);
         ctx.font = "10px Verdana";
         ctx.textAlign = "center";
         ctx.fillText(`${(1.5-(now-dash.lastEnded)/1000).toFixed(2)}s`, cnv.width-100, cnv.height/2+10/3);
@@ -119,19 +138,20 @@ function draw() {
     ctx.fillStyle = "#FF000050";
     ctx.strokeStyle = "#FF0000";
     ctx.lineWidth = 2.5;
-    ctx.fillRect(cnv.width*3/4+mapX, cnv.height/2-50+mapY, 100, 100);
-    ctx.drawImage(document.getElementById("sword-icon"), cnv.width*3/4+mapX, cnv.height/2-50+mapY, 100, 100);
-    ctx.strokeRect(cnv.width*3/4+mapX, cnv.height/2-50+mapY, 100, 100);
-    let distSword = Math.hypot(player.x - (cnv.width*3/4+50+mapX), player.y - (cnv.height/2+mapY));
+    ctx.fillRect(cnv.width*0.75+mapX, cnv.height*0.5-50+mapY, 100, 100);
+    ctx.drawImage(document.getElementById("sword-icon"), cnv.width*0.75+mapX, cnv.height*0.5-50+mapY, 100, 100);
+    ctx.strokeRect(cnv.width*0.75+mapX, cnv.height*0.5-50+mapY, 100, 100);
+    let distSword = Math.hypot(player.x - (cnv.width*0.75+50+mapX), player.y - (cnv.height*0.5+mapY));
     if (distSword < 150) {
         ctx.lineWidth = 1.25;
-        ctx.fillRect(cnv.width*3/4-5+mapX, cnv.height/2+60+mapY, 110, 20);
-        ctx.strokeRect(cnv.width*3/4-5+mapX, cnv.height/2+60+mapY, 110, 20);
+        if (mouseover.equipSword) ctx.fillStyle = "#FF000025";
+        ctx.fillRect(cnv.width*0.75-5+mapX, cnv.height*0.5+60+mapY, 110, 20);
+        ctx.strokeRect(cnv.width*0.75-5+mapX, cnv.height*0.5+60+mapY, 110, 20);
         
         ctx.fillStyle = "#FF0000";
         ctx.textAlign = "center";
         ctx.font = "bold 15px Verdana";
-        ctx.fillText("Equip Sword", cnv.width*3/4+50+mapX, cnv.height/2+75+mapY);
+        ctx.fillText("Equip Sword", cnv.width*0.75+50+mapX, cnv.height*0.5+75+mapY);
     }
 
     // Player
